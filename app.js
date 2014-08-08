bb2c33f1060aaca2979cbc904a3e030955651bb5
David Pitard <david.pitard@safenet-inc.com>
2014-08-08 15:34:07 -0400

    Updated to use promises

--- app.js
+++ app.js
@@ -3,7 +3,6 @@
  */
 
 var express = require('express');
-var request = require('request');
 var app = express();
 var aasguard = require('aasguard');
 
@@ -13,15 +12,19 @@ app.get('/user_keys', function(req, res) {
 
 app.get('/user_keys/:id', function(req, res) {
     var id;
-    aasguard.get_user(req.params.id, function(user, error){
-        if (error){
-            aasguard.create_user(req.params.id, "friendly_"+req.params.id, function(user, error){
-                return res.json(user)
-            })
-        } else {
-            return res.json(user);
-        }
-    });
+    aasguard.get_user(req.params.id)
+        .then(function(data){
+            res.json(data);
+         })
+        .catch(function(e) {
+            aasguard.create_user(req.params.id, "friendly_"+req.params.id)
+                .then(function(data){
+                    res.json(data)
+                })
+                .catch(function(e){
+                    res.send(e.res.statusCode, e.message);
+                });
+        });
 });
 
 aasguard.set_token("slZxxbJai9hOWyRRYBrMWA");
